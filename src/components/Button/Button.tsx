e57a725af5a724260493277772f3afcae690dceb
Wesley Janse <wesleyjanse@gmail.com>
2021-12-30 01:57:29 +0100

    Fixed jest scss complains

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,13 +1,13 @@
 import "../../styles/theme.scss";
 import React from "react";
-import button from "./Button.module.scss";
+import styles from "./Button.module.scss";
 
 export interface ButtonProps {
   label: string;
 }
 
 const Button = (props: ButtonProps) => {
-  return <button className={button}>{props.label} hehe </button>;
+  return <button className={styles.button}>{props.label}</button>;
 };
 
 export default Button;
